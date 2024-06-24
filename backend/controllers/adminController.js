2ef5166c411bd44f991702bd631728ca11404cd2
dawit2123 <dawitzewdu2123@gmail.com>
2024-06-24 16:47:37 +0300

    adminconroller change password field added

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,14 +1,18 @@
-import AdminJs from "adminjs";
+import AdminJs, { Edit } from "adminjs";
 import AdminJsExpress from "@adminjs/express";
 import * as AdminJsMongoose from "@adminjs/mongoose";
+import passwordsFeature from "@adminjs/passwords";
 import path from "path";
+import bcrypt from "bcryptjs";
 import { Music } from "../models/musics/musicModel.js";
 import User from "../models/users/userModel.js";
 import { fileURLToPath } from "url";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 import dotenv from "dotenv";
 
+// Directory setup
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// Load environment variables
 const result = dotenv.config({
   path: path.resolve(`${__dirname}../../`, `config.env`),
 });
@@ -18,6 +22,7 @@ AdminJs.registerAdapter({
   Resource: AdminJsMongoose.Resource,
 });
 
+// Define the AdminJS instance with resources
 const adminJs = new AdminJs({
   resources: [
     {
@@ -26,12 +31,27 @@ const adminJs = new AdminJs({
         parent: {
           name: "User Management",
         },
+        properties: {
+          properties: { password: { isVisible: false } },
+        },
         listProperties: [
           "firstName",
           "lastName",
           "profileImage",
           "email",
           "role",
+        ],
+        showProperties: ["firstName", "lastName", "email", "role"],
+        editProperties: ["firstName", "lastName", "email", "role", "password"],
+        features: [
+          passwordsFeature({
+            properties: {
+              componentLoader,
+              encryptedPassword: "password",
+              password: "password",
+            },
+            hash: (password) => bcrypt.hash(password, 12),
+          }),
         ],
       },
     },
@@ -42,16 +62,29 @@ const adminJs = new AdminJs({
           name: "Music Management",
         },
         listProperties: ["title", "artistName", "duration"],
+        editProperties: ["title", "artistName", "duration"],
       },
     },
   ],
   rootPath: "/admin",
 });
 
+// Admin credentials
 const ADMIN = {
   email: process.env.ADMIN_EMAIL,
   password: process.env.ADMIN_PASSWORD,
 };
 
-const router = AdminJsExpress.buildRouter(adminJs);
+// Build and export the router
+const router = AdminJsExpress.buildAuthenticatedRouter(adminJs, {
+  authenticate: async (email, password) => {
+    if (email === ADMIN.email && password === ADMIN.password) {
+      return ADMIN;
+    }
+    return null;
+  },
+  cookieName: process.env.ADMIN_COOKIE_NAME,
+  cookiePassword: process.env.ADMIN_COOKIE_PASSWORD,
+});
+
 export { adminJs, router };
